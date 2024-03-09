4eff2e9b38c666f65ec7062513547a75c6f527ed
juans.cespedes <juans.cespedes@LSTKLM290470>
2024-03-09 16:50:08 -0500

    Modificación de tipo de dato (IdCaso) - NOTIFICACION_USUARIO

--- AgroSistema.Application/Notificacion/ListarNotificaciones/ListarNotificacionesCommand.cs
+++ AgroSistema.Application/Notificacion/ListarNotificaciones/ListarNotificacionesCommand.cs
@@ -10,6 +10,6 @@ namespace AgroSistema.Application.Notificacion.ListarNotificaciones
     public class ListarNotificacionesCommand : IRequest<IEnumerable<ListarNotificacionesDTO>>
     {
         public int IdUsuario { get; set; }
-        public int IdCaso { get; set; }
+        public string? IdCaso { get; set; }
     }
 }
